9dac51d79ba600791ee4f8b92db3330d2081bb2d
Lara Raad <lara.raad@gmail.com>
2020-03-09 15:16:53 +0100

    Update merge_mask.m

--- merge_mask.m
+++ merge_mask.m
@@ -4,6 +4,7 @@
 %it under the terms of the GNU Affero General Public License as
 %published by the Free Software Foundation, either version 3 of the
 %License, or (at your option) any later version.
+%
 %You should have received a copy of the GNU Affero General Public License
 %along with this program. If not, see <http://www.gnu.org/licenses/>. 
 
